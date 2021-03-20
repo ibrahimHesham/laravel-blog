8e04d1c260fe03de29ca9bebf6381f1797e1b296
Ibrahim-Hesham <ibrahim.hesham.cs@gmail.com>
2021-03-20 15:27:48 +0200

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public $posts = [
+        ['id' => 1, 'title' => 'post 1', 'posted_by' => 'Ibrahim', 'created_at' => '2021-03-20', 'description' => 'here is first post description', 'Email' => 'ibrahim@gmail.com'],
+        ['id' => 2, 'title' => 'post 2', 'posted_by' => 'yehia', 'created_at' => '2021-04-15'],
+        ['id' => 3, 'title' => 'post 3', 'posted_by' => 'Ali', 'created_at' => '2021-06-01'],
+    ];
+    public function index()
+    {
+        
+        return view('posts.index', [
+            'posts' => $this->posts
+        ]);
+    }
+
+    public function show($postId)
+    {
+        $post = ['id' => 1, 'title' => 'post 1', 'posted_by' => 'Ibrahim', 'created_at' => '2021-03-20', 'description' => 'here is first post description', 'Email' => 'ibrahim@gmail.com'];
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <title></title>
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">All posts <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+<div class="container mt-5">
+
+<button type="button" class="btn btn-success">Create</button>
+
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+      <th scope="col">posted_by</th>
+      <th scope="col">created_at</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    @foreach($posts as $post)
+    <tr>
+      <th scope="row">{{$post['id']}}</th>
+      <td>{{$post['title']}}</td>
+      <td>{{$post['posted_by']}}</td>
+      <td>{{$post['created_at']}}</td>
+      <td>
+      <button type="button" class="btn btn-info">View</button>
+      <button type="button" class="btn btn-primary">Edit</button>
+      <button type="button" class="btn btn-danger">Delete</button>
+      </td>
+    </tr>
+
+    @endforeach
+    <!-- <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr> -->
+  </tbody>
+</table>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
